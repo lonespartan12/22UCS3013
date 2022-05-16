7029d86815e6bba52d184e011511123b4c4cd5af
Thyago Mota <thyagomota@gmail.com>
2022-05-16 16:13:43 -0600

    updates

---
+++ activity_01_greet/MainActivity.kt
@@ -0,0 +1,29 @@
+package edu.msudenver.greet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    // NOTE: following variable declaration statements were added
+    private lateinit var nameText: EditText
+    private lateinit var greetButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // NOTE: following statements were added
+        nameText = findViewById(R.id.name_text)
+        greetButton = findViewById(R.id.greet_button)
+        greetButton.setOnClickListener {
+            view: View ->
+            Toast.makeText(this, "Hello " + nameText.text + "!", Toast.LENGTH_LONG).show()
+        }
+    }
+}
