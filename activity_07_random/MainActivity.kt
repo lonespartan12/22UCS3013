c213a054e0cb3d133986d65beb9db45ac6991992
Thyago Mota <thyagomota@gmail.com>
2022-05-23 13:39:27 -0600

    updates

---
+++ activity_07_random/MainActivity.kt
@@ -0,0 +1,26 @@
+package edu.msudenver.randomnumbergenerator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // TODO: get a reference to the textview
+
+        // TODO: write the for the "onClickListener"
+        // 1a get a reference to the edittext "from"
+        // 1b extract the "from" integer value
+        // 2a get a reference to the edittext "to"
+        // 2b extract the "to" integer value
+        // 3 generate a random number in "from" until "to" + 1
+        // 4 update the textview
+        
+    }
+}
