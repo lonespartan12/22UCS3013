2323ab2ba6433dcd4320aa591ec7bf7fbb255fd7
lonespartan12 <lonespartan12@hotmail.com>
2022-06-13 15:03:22 -0600

    LinearLayout Complete

---
+++ activity_04_linearlayout/app/src/main/java/com/activity_00_22ucs3013/android/activity_04_linearlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.activity_00_22ucs3013.android.activity_04_linearlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
