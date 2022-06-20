9172fb0c072ec1d1aab7f6f22e72425fcd25b4a4
lonespartan12 <lonespartan12@hotmail.com>
2022-06-20 12:48:58 -0600

    update path for submission

--- HW3/app/src/main/java/com/activity_00_22ucs3013/android/myapplication/MainActivity.kt
+++ HW3/app/src/main/java/com/activity_00_22ucs3013/android/myapplication/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.activity_00_22ucs3013.android.myapplication
-//package edu.msudenver.number_guessing // remove comment when grading
-//package com.activity_00_22ucs3013.android.hwk_03_number_guessing // comment out when grading
+package edu.msudenver.number_guessing // remove comment when grading
+//package com.activity_00_22ucs3013.android.myapplication // comment out when grading
 
 /*
  * CS3013 - Mobile App Dev. - Summer 2022
